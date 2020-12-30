88129ea754694e4a552a241cada85f21f3bbdc9d
Liz <lizriderwilson@gmail.com>
2020-12-30 14:19:42 -0500

    Test behavior of gets without .chomp

--- ex11.rb
+++ ex11.rb
@@ -1,8 +1,8 @@
 print "How old are you? "
-age = gets.chomp
+age = gets
 print "How tall are you? "
-height = gets.chomp
+height = gets
 print "How much do you weight? "
-weight = gets.chomp
+weight = gets
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
