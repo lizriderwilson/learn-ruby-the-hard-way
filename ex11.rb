d0e37c34a0f3a83236a28ed52639e63e9f5e6de0
Liz <lizriderwilson@gmail.com>
2020-12-30 14:23:53 -0500

    Test gets.chomp.to_i

--- ex11.rb
+++ ex11.rb
@@ -1,8 +1,9 @@
 print "How old are you? "
-age = gets
-print "How tall are you? "
-height = gets
-print "How much do you weight? "
-weight = gets
+age = gets.chomp.to_i
+# print "How tall are you? "
+# height = gets
+# print "How much do you weight? "
+# weight = gets
 
-puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+# puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+puts "Nex year, you will turn #{age + 1}."
