f208383f70604e3244d36dc18b889672cb4044ce
Liz <lizriderwilson@gmail.com>
2020-12-31 11:32:04 -0500

    Complete study drills

--- ex12.rb
+++ ex12.rb
@@ -1,12 +1,17 @@
-print "Give me a number: "
-number = gets.chomp.to_i
+# print "Give me a number: "
+# number = gets.chomp.to_i
 
-bigger = number * 100
-puts "A bigger number is #{bigger}"
+# bigger = number * 100
+# puts "A bigger number is #{bigger}"
 
-print "Give me another number: "
-another = gets.chomp
-number = another.to_i
+# print "Give me another number: "
+# another = gets.chomp
+# number = another.to_i
 
-smaller = number / 100
-puts "A smaller number is #{smaller}."
+# smaller = number / 100
+# puts "A smaller number is #{smaller}."
+
+puts "Give me some money!"
+money = gets.chomp.to_f
+
+puts "#{(money / 10).round(2)} is your change."
